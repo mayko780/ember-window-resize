4413850daa88f9a25bd556fb5196cac5dd462c41
Mirko May <mail@mirkomay.com>
2017-11-28 19:03:53 +1300

    Update initializer

--- addon/initializers/resize.js
+++ addon/initializers/resize.js
@@ -2,7 +2,7 @@ import ResizeService from 'ember-window-resize/services/resize';
 
 export function initialize(application) {
   application.register('service:resize', ResizeService);
-  application.inject('service', 'resizeService', 'service:resize');
+  // application.inject('service', 'resizeService', 'service:resize');
   application.inject('route', 'resizeService', 'service:resize');
   application.inject('template', 'resizeService', 'service:resize');
   application.inject('component', 'resizeService', 'service:resize');
