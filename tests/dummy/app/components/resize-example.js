97a2e6769104aa91b6307b58e88467cf8a81aff1
Mirko May <mail@mirkomay.com>
2020-05-08 14:45:27 +1200

    Fixed depricated Ember logger in dummy app

--- tests/dummy/app/components/resize-example.js
+++ tests/dummy/app/components/resize-example.js
@@ -1,17 +1,15 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import layout from '../templates/components/resize-example';
-import Ember from 'ember';
-
-const { Logger } = Ember;
 
 export default Component.extend({
   layout,
   windowResize: service(),
   didInsertElement() {
     this._super(...arguments);
-    this.get('windowResize').on('didResize', (event) => {
-      Logger.info('resize', event);
+    this.windowResize.on('didResize', (event) => {
+      // eslint-disable-next-line no-console
+      console.info('resize', event);
     });
   }
 });
